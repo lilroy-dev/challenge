7e8fb7be5f950dd3f9bbf8aec3ab28f361614f35
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-09-10 21:01:07 +0300

    @ week 2.1 @ memoize function

--- week#2/task_1/solution.js
+++ week#2/task_1/solution.js
@@ -1,1 +1,26 @@
 // solution #1
+const slowFunction = (num) => {
+    console.log('Computing...');
+    return num * 2;
+};
+
+function memoize (func) {
+    const cache = new Map();
+    return function (...arg) {
+       const key = JSON.stringify(arg);
+       if (cache.has(key)) {
+           console.log('from cache');
+           return cache.get(key);
+       }
+       const result = func(...arg);
+       cache.set(key, result);
+       return result;
+    }
+}
+
+const memoizedFunction = memoize(slowFunction);
+
+console.log(memoizedFunction(5)); // Computing... 10
+console.log(memoizedFunction(5)); // 10 (из кэша)
+console.log(memoizedFunction(10)); // Computing... 20
+console.log(memoizedFunction(10)); // 20 (из кэша)
