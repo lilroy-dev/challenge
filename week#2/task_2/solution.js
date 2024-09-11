f1a0b765eddd4fc07891fc494cf9ba86750fff9c
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-09-11 10:27:58 +0300

    @ week 2.1 @ check anagrams function

---
+++ week#2/task_2/solution.js
@@ -0,0 +1,16 @@
+const areAnagrams = (str1, str2) => {
+    const arr_1 = str1.split('').sort();
+    const arr_2 = str2.split('').sort();
+
+    if (arr_1.length !== arr_2.length) return false;
+
+    for (let i = 0; i < arr_1.length; i++) {
+        if (arr_1[i] !== arr_2[i]) return false;
+    }
+    return true
+}
+
+
+
+console.log(areAnagrams('listen', 'silent')); // true
+console.log(areAnagrams('hello', 'world')); // false
